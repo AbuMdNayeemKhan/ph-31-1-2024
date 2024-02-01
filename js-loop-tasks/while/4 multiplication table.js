bcf0d48ea8e6d87bf1f56193a5148695585ca96f
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-01 08:46:04 +0600

    while loop done almost

--- js-loop-tasks/while/4 multiplication table.js
+++ js-loop-tasks/while/4 multiplication table.js
@@ -1,8 +1,8 @@
 /***
-
 As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
-
  */
-
-
 /*programming hero*/
+
+for(let i = 1; i < 11; i++){
+    console.log(`5 x ${i} = ${5 * i}`);
+}
